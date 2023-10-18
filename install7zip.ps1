bd39f8ee518c65fdbdab7bce57ed9e189f2305de
LivnJonWill <148148966+LivnJonWill@users.noreply.github.com>
2023-10-18 15:32:45 -0500

    Add files via upload

---
+++ install7zip.ps1
@@ -0,0 +1,11 @@
+﻿
+#Write-host "Changing to install mode"
+#Change user /install
+
+Write-host "Installing 7zip" 
+start-process -FilePath "$destination\7z2301-x64.exe" -ArgumentList “/S" -Wait
+
+
+#Write-host "Changing to execute mode"
+#Change user /execute
+
