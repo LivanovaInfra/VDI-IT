d3d663dc0170b6e79c6a6c845c452afb9d9a09c4
LivnJonWill <148148966+LivnJonWill@users.noreply.github.com>
2024-02-16 10:12:05 -0600

    Update install7zip.ps1
    Added write-host commands so it shows up in the Azure custom image build log.

--- install7zip.ps1
+++ install7zip.ps1
@@ -1,4 +1,5 @@
 #Changing to install mode.
+Write-host "Changing user mode to install."
 Change user /install
 
 #Declare installation source.
@@ -11,10 +12,13 @@ $dest = "C:\Temp\"
 $7Zip = '7z2301-x64.exe'
 
 #Download binaries
+Write-host "Downloading 7zip binaries."
 Invoke-WebRequest -uri ($SourceURL + $7Zip) -OutFile ($dest + $7Zip)
 
 #Install 7zip.
+Write-host "Installing 7zip."
 start-process -FilePath ($dest + $7Zip) -ArgumentList “/S" -Wait
 
 #Write-host "Changing to execute mode"
+Write-host "Changing usermode to execute."
 Change user /execute
