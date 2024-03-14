87c38ce5f77b119083b8b48f9043552d7e12fac9
LivnJonWill <148148966+LivnJonWill@users.noreply.github.com>
2024-03-14 14:50:45 -0500

    Add files via upload

---
+++ installPowerBIDesktop.ps1
@@ -0,0 +1,16 @@
+﻿#Declare installation source.
+$SourceURL = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/'
+
+#Declase Destination to download binaries.
+$dest = "C:\Temp\"
+
+#Declare Installation Binaries
+$App = 'PBIDesktopSetup_x64.exe'
+
+#Download binaries
+Write-host "Downloading $App binaries."
+Invoke-WebRequest -uri ($SourceURL + $App) -OutFile ($dest + $App)
+
+#Install App.
+Write-host "Installing $App."
+start-process -FilePath ($dest + $App) -ArgumentList "-S ACCEPT_EULA=1" -Wait
